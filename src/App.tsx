c6481dd84d1f33d332d7b8ec00707cbfd733a00c
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-08-11 20:54:35 +0200

    Alten Code entfernt

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-import { useQuery } from "react-query";
-
-import { fetchOverview } from "./fetchers";
 import Create from "./create/Create";
 import Settings from "./appSettings/Settings";
 import Select from "./select/Select";
@@ -9,21 +5,6 @@ import Edit from "./edit/Edit";
 import Menu from "./components/Menu";
 
 export default function App() {
-    // const { data, status } = useQuery("overview", fetchOverview);
-
-    // if (status === "loading") return <div>Loading...</div>;
-    // if (status === "error") return <div>Error!</div>;
-
-    // <div>{ JSON.stringify(data) }</div>
-
-    // <div>
-            
-            
-    //         <div>Frage auswählen</div>
-    //         <div>Einstellungen</div>
-    //         <div>Fragen erstellen</div>
-    //     </div>
-
     return (
         <Menu options={[
             { name: "Frage oder Quiz auswählen", component: <Select />},
