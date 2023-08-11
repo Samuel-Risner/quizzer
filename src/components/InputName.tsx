bafbbaec34012100b01f98843a9a1d8054fa922d
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-08-11 20:53:11 +0200

    Component um die Namen zu überprüfen hinzugefügt

---
+++ src/components/InputName.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+
+type Props = {
+    fetchNamesFunction: () => Promise<string[]>;
+    presetName?: string;
+    setName: (name: string) => void;
+}
+
+export default function InputName({ fetchNamesFunction, presetName="", setName }: Props) {
+    const [ name, changeName ] = useState<string>(presetName);
+    const [ nameExists, changeNameExists ] = useState<boolean | null>(null);
+    const { data, status } = useQuery("overview", fetchNamesFunction);
+
+    if (status === "loading") return <div>Bereits vorhandene Namen werden geladen...</div>;
+    if ((status === "error") || (data === undefined)) return <div>Die bereits vorhandenen Namen konnten nicht geladen werden.</div>;
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const newName = formData.get("name") as string;
+        
+        if (newName === null) return;
+        if (data.includes(newName)) {
+            changeNameExists(true);
+            return;
+        }
+
+        changeNameExists(false);
+        changeName(newName);
+        setName(name);
+    }
+
+    let nameExistsJSX: JSX.Element = <></>;
+
+    if (nameExists === true) {
+        nameExistsJSX = <div className="text-red-500">Der Name "{ name }" existiert bereits!</div>
+    } else if (nameExists === false) {
+        nameExistsJSX = <div className="text-green-500">Der Name "{ name }" kann verwendet werden.</div>
+    } else {
+        nameExistsJSX = <></>;
+    }
+
+    return (
+        <div>
+            <form onSubmit={ handleSubmit }>
+                <label>
+                    Bitte den Namen eingeben:
+                    <input defaultValue={ name } type="text" name="name" />
+                </label>
+                <button type="submit">überprüfen</button>
+                { nameExistsJSX }
+            </form>
+        </div>
+    );
+}
