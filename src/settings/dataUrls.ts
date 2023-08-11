1bea8fad53b2710ba99a172e9b3cc72ee2b42d63
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-08-11 20:52:33 +0200

    Pfade für die gespeicherten Namen hinzugefügt

--- src/settings/dataUrls.ts
+++ src/settings/dataUrls.ts
@@ -3,5 +3,10 @@ import general from "./general";
 export default {
     quizzes: general.rootUrl + "Quizze",
     multipleChoice: general.rootUrl + "Fragen/MultipleChoice",
-    trueOrFalse: general.rootUrl + "Fragen/WahrOderFalsch"
+    trueOrFalse: general.rootUrl + "Fragen/WahrOderFalsch",
+    names: {
+        quizzes: general.rootUrl + "Namen/quizze.json",
+        multipleChoice: general.rootUrl + "Namen/multipleChoice.json",
+        trueOrFalse: general.rootUrl + "Namen/wahrOderFalsch.json"
+    }
 };
