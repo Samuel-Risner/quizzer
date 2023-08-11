4124c37a3137d0970fe6a0eb4fc5dec0629b0334
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-08-11 20:53:48 +0200

    Component um Namen zu überprüfen eingefügt

--- src/editOrCreate/trueOrFalse/TrueOrFalse.tsx
+++ src/editOrCreate/trueOrFalse/TrueOrFalse.tsx
@@ -1,17 +1,12 @@
+import InputName from "../../components/InputName";
 import UseImage from "../../components/UseImage";
+import { fetchNamesTrueOrFalse } from "../../fetchers";
 import SelectAppearance from "./SelectAppearance";
-
-type ImageData = {
-    url: string;
-    altText: string;
-    info: string;
-}
-
-const imageData: ImageData[] = [];
 
 export default function TrueOrFalse() {
     return (
         <div>
+            <InputName fetchNamesFunction={ fetchNamesTrueOrFalse } setName={ (name: string) => {}} />
             <SelectAppearance />
             <input type="text" />
             <UseImage />
