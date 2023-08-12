3b6559ef965181ca2aa20b739876acaa8f47406e
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-08-12 18:27:17 +0200

    "useState" statt "DataHandler" verwendet

--- src/editOrCreate/trueOrFalse/TrueOrFalse.tsx
+++ src/editOrCreate/trueOrFalse/TrueOrFalse.tsx
@@ -4,18 +4,28 @@ import InputName from "../../components/InputName";
 import UseImage from "../../components/UseImage";
 import { fetchNamesTrueOrFalse } from "../../fetchers";
 import SelectAppearance from "./SelectAppearance";
-import TrueOrFalseDataHandler from "../../dataHandlers/trueOrFalse";
-import InputOptions from "./InputOptions";
+import Save from "../../components/Save";
+import InputQuestion from "./inputQuestion";
 
 export default function TrueOrFalse() {
-    const dataHandler = new TrueOrFalseDataHandler();
+    const [ questionName, setQuestionName ] = useState<string>("");
+    const [ appearanceIndex, setAppearanceIndex ] = useState<number>(0);
+    const [ question, setQuestion ] = useState<string>("");
+    const [ imageUrl, setImageUrl ] = useState<string>("");
+    const [ useImage, setUseImage ] = useState<boolean>(false);
+
+    const changeQuestionName = (name: string) => { setQuestionName(name); }
+    const changeAppearanceIndex = (index: number) => { setAppearanceIndex(index); }
+    const changeQuestion = (question: string) => { setQuestion(question); }
+    const changeImageUrl = (url: string) => { setImageUrl(url); }
+    const changeUseImage = (use: boolean) => { setUseImage(use); }
 
     return (
         <div>
-            <InputName fetchNamesFunction={ fetchNamesTrueOrFalse } dataHandler={ dataHandler } />
-            <SelectAppearance dataHandler={ dataHandler } />
-            <InputOptions dataHandler={ dataHandler } />
-            <UseImage dataHandler={ dataHandler } />
+            <InputName name={ questionName } changeName={ changeQuestionName } fetchNamesFunction={ fetchNamesTrueOrFalse } />
+            <SelectAppearance appearanceIndex={ appearanceIndex } changeAppearanceIndex={ changeAppearanceIndex } />
+            <InputQuestion question={ question } changeQuestion={ changeQuestion } />
+            <UseImage url={ imageUrl } changeUrl={ changeImageUrl } useImage={ useImage } changeUseImage={ changeUseImage } />
         </div>
     );
 }
