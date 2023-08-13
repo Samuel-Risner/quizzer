fc3a205378296e76e0eaf18770099c8ce3aa8959
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-08-13 13:36:15 +0200

    Angefangen die Sachen für Multiple Choice Fragen hinzuzufügen

--- src/create/CreateQuestion.tsx
+++ src/create/CreateQuestion.tsx
@@ -1,10 +1,12 @@
 import Menu from "../components/Menu";
+import MultipleChoice from "../editOrCreate/multipleChoice/MultipleChoice";
 import TrueOrFalse from "../editOrCreate/trueOrFalse/TrueOrFalse";
 
 export default function CreateQuestion() {
     return (
         <Menu options={[
-            { name: "Wahr oder Falsch", component: <TrueOrFalse /> }
+            { name: "Wahr oder Falsch", component: <TrueOrFalse /> },
+            { name: "Multiple Choice", component: <MultipleChoice />}
         ]} />
     );
 }
---
+++ src/editOrCreate/multipleChoice/MultipleChoice.tsx
@@ -0,0 +1,7 @@
+export default function MultipleChoice() {
+    return (
+        <div>
+            Multiple Choice
+        </div>
+    );
+}
